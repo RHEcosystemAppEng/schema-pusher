64368c58f9b5b6035aa3b53da8b904e72c1558e5
Tomer Figenblat <tomer.figenblat@gmail.com>
2022-01-02 10:30:27 +0200

    test: small refactoring

--- src/test/java/com/redhat/schema/pusher/avro/Avro_push_cli_implemenetation_Test.java
+++ src/test/java/com/redhat/schema/pusher/avro/Avro_push_cli_implemenetation_Test.java
@@ -8,7 +8,6 @@ import static org.springframework.test.util.ReflectionTestUtils.getField;
 import com.redhat.schema.pusher.NamingStrategy;
 import com.redhat.schema.pusher.SchemaPusher;
 import java.net.URISyntaxException;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.logging.Level;
@@ -30,14 +29,11 @@ class Avro_push_cli_implemenetation_Test {
   private static final String DIRECTORY = "com/redhat/schema/pusher/avro/schemas/subfolder";
   private static final NamingStrategy FAKE_NAMING_STRATEGY = NamingStrategy.TOPIC_RECORD;
 
-  private Path expectedFile;
   @Mock private ApplicationContext mockContext;
   @InjectMocks private AvroPushCli sut;
 
   @BeforeEach
   void initialize() throws URISyntaxException {
-    // the target directory has only one file
-    expectedFile = Paths.get(getClass().getClassLoader().getResource(DIRECTORY + "/test_schema_too.json").toURI());
     var directoryAbs = Paths.get(getClass().getClassLoader().getResource(DIRECTORY).toURI());
     // parse the command line arguments for the sut
     new CommandLine(sut).parseArgs(
@@ -51,15 +47,17 @@ class Avro_push_cli_implemenetation_Test {
 
   @Test
   void executing_the_cli_implementation_should_invoke_the_schmea_pusher_implementation(
-      @Mock final SchemaPusher mockSchemaPusher) {
+      @Mock final SchemaPusher mockSchemaPusher) throws URISyntaxException {
     // turn off the sut's logger to avoid polluting the build log
     ((Logger) getField(sut, "LOGGER")).setLevel(Level.OFF);
+    // given the target directory has only one file
+    var expectedFile = Paths.get(getClass().getClassLoader().getResource(DIRECTORY + "/test_schema_too.json").toURI());
     // given the mocked di context will return  the mock schema pusher per the arguments
     given(mockContext.getBean(
       SchemaPusher.class, FAKE_BOOTSTRAP, FAKE_REGISTRY, FAKE_NAMING_STRATEGY)).willReturn(mockSchemaPusher);
     // when the sut executes
     sut.run();
-    // then the mock pusher should be pushed with the expected arguments
+    // then the mock pusher should be pushed with the fake topic and the testing schema file path
     then(mockSchemaPusher).should().push(eq(List.of(FAKE_TOPIC)), eq(List.of(expectedFile)));
   }
 }
