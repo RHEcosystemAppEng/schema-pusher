4aa3374a21d16f256cddb306c24eb6efcd9c8a59
Tomer Figenblat <tomer.figenblat@gmail.com>
2022-01-03 09:20:08 +0200

    test: refactor test case

--- src/test/java/com/redhat/schema/pusher/Verify_manifest_version_provider_utility_Test.java
+++ src/test/java/com/redhat/schema/pusher/Verify_manifest_version_provider_utility_Test.java
@@ -6,8 +6,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class Verify_manifest_version_provider_utility_Test {
   @Test
-  void fetching_the_version_info_without_a_manifest_should_return_the_default_values() throws Exception {
+  void fetching_the_version_info_with_a_testing_manifest_should_return_the_expected_version() throws Exception {
     var sut = new ManifestVersionProvider();
-    assertThat(sut.getVersion()).containsOnly("Default Name 0.0.0");
+    assertThat(sut.getVersion()).containsOnly("Fake Application Name 1.2.3-FAKE");
   }
 }
