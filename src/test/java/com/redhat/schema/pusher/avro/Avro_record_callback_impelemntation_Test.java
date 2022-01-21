14592edc7fd9c65a97b84818a26274cc4d16cda9
Tomer Figenblat <tomer.figenblat@gmail.com>
2022-01-21 12:27:25 +0200

    test: added and fixed existing tests after latest user ca feature

---
+++ src/test/java/com/redhat/schema/pusher/avro/Avro_record_callback_impelemntation_Test.java
@@ -0,0 +1,48 @@
+package com.redhat.schema.pusher.avro;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.then;
+
+import java.util.function.Supplier;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class Avro_record_callback_impelemntation_Test {
+  private static final String FAKE_TOPIC = "faketopic";
+  private static final String FAKE_FILENAME = "fake_schmea_file.json";
+
+  @Mock private Logger mockLogger;
+  @Mock private RecordMetadata mockRecordMetadata;
+  private AvroRecordCallbak sut;
+
+  @BeforeEach
+  void initialize() {
+    sut = new AvroRecordCallbak(mockLogger, FAKE_FILENAME, FAKE_TOPIC);
+  }
+
+  @SuppressWarnings("unchecked")
+  @Test
+  void invoking_on_completion_with_a_null_exception_should_log_an_info_message() {
+    // when invoking the onCompletion overided method with the sut
+    sut.onCompletion(mockRecordMetadata, null);
+    then(mockLogger).should().info(any(Supplier.class));
+  }
+
+  @SuppressWarnings("unchecked")
+  @Test
+  void invoking_on_completion_with_a_non_null_exception_should_log_a_severe_message() {
+    // given the following exception
+    var expectedException = new Exception("fake execption");
+    // when invoking the onCompletion overided method with the sut
+    sut.onCompletion(mockRecordMetadata, expectedException);
+    then(mockLogger).should().log(eq(Level.SEVERE), eq(expectedException), any(Supplier.class));
+  }
+}
--- src/test/java/com/redhat/schema/pusher/avro/Avro_schema_pusher_implementation_Test.java
+++ src/test/java/com/redhat/schema/pusher/avro/Avro_schema_pusher_implementation_Test.java
@@ -4,12 +4,12 @@ package com.redhat.schema.pusher.avro;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.inOrder;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.util.ReflectionTestUtils.getField;
@@ -19,20 +19,20 @@ import com.redhat.schema.pusher.NamingStrategy;
 import com.redhat.schema.pusher.PushCli;
 import com.redhat.schema.pusher.PushCli.KeystoreInfo;
 import com.redhat.schema.pusher.PushCli.TruststoreInfo;
+import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.net.URISyntaxException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.Properties;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
 import java.util.function.Predicate;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.apache.avro.generic.IndexedRecord;
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -59,6 +59,7 @@ class Avro_schema_pusher_implementation_Test {
   private static final String FAKE_KEYSTORE_PASSWORD = "hidemetoo123#@!";
 
   @Captor private ArgumentCaptor<ProducerRecord<String, IndexedRecord>> prodRecCaptore;
+  @Mock private Callback mockCallback;
   @Mock private KafkaProducer<String, IndexedRecord> mockProducer;
   @Mock private ApplicationContext mockContext;
   @Mock private PushCli mockCli;
@@ -66,8 +67,7 @@ class Avro_schema_pusher_implementation_Test {
 
   @Test
   @SuppressWarnings("unchecked")
-  void a_failed_push_should_not_throw_an_exception(@Mock final Future<RecordMetadata> mockFuture)
-      throws URISyntaxException, InterruptedException, ExecutionException {
+  void a_failed_push_with_the_producer_should_not_throw_an_exception() throws URISyntaxException {
     // stub the cli
     when(mockCli.getKafkaBootstrap()).thenReturn(FAKE_SECURED_BOOTSTRAP);
     when(mockCli.getServiceRegistry()).thenReturn(FAKE_REGISTRY);
@@ -76,22 +76,23 @@ class Avro_schema_pusher_implementation_Test {
     sut = new AvroSchemaPusher(mockCli);
     // stub the private di context
     setField(sut, "context", mockContext);
+    // stub the mocked context to return a mocked callback for the specific arguments
+    given(mockContext.getBean(eq(Callback.class), any(Logger.class), eq("test_schema.avsc"), eq(FAKE_TOPIC1))).willReturn(mockCallback);
     // turn off the sut's logger to avoid polluting the build log
     ((Logger) getField(sut, "LOGGER")).setLevel(Level.OFF);
-    // short-circuit the future's get method to throw an exception
-    given(mockFuture.get()).willThrow(new InterruptedException());
-    // stub the producer with a mocked future for any send methd invocation
-    given(mockProducer.send(any(ProducerRecord.class))).willReturn(mockFuture);
+    // short-circuit the producer to throw an exception
+    given(mockProducer.send(any(ProducerRecord.class), eq(mockCallback))).willThrow(new UncheckedIOException(new IOException("fake exception")));
     // given the di context will return the mocked producer as bean per the properties match
     given(mockContext.getBean(eq(KafkaProducer.class), argThat(securedPropertiesMatcher))).willReturn(mockProducer);
-    // given the following non existing schema file
+    // given the following test schema
     var testSchema = getResourceAbsPath("com/redhat/schema/pusher/avro/schemas/test_schema.avsc");
     // when invoking the push method with it no exceptions should be thrown
     assertThatNoException().isThrownBy(() -> sut.push(List.of(FAKE_TOPIC1), List.of(testSchema)));
+    // and the mock producer was invoked
+    then(mockProducer).should().send(any(ProducerRecord.class), eq(mockCallback));
   }
 
   @Test
-  @SuppressWarnings("unchecked")
   void pushing_one_file_and_one_topic_should_result_in_one_producer_records_sent(
       @Mock final TruststoreInfo mockTruststoreInfo,
       @Mock final KeystoreInfo mockKeystoreInfo) throws URISyntaxException {
@@ -113,22 +114,21 @@ class Avro_schema_pusher_implementation_Test {
     ((Logger) getField(sut, "LOGGER")).setLevel(Level.OFF);
     // stub the private di context
     setField(sut, "context", mockContext);
-    // stub the producer with a mocked future for any send methd invocation
-    given(mockProducer.send(any(ProducerRecord.class))).willReturn(mock(Future.class));
+    // stub the mocked context to return a mocked callback for the specific arguments
+    given(mockContext.getBean(eq(Callback.class), any(Logger.class), eq("test_schema.avsc"), eq(FAKE_TOPIC1))).willReturn(mockCallback);
     // given the di context will return the mocked producer as bean per the properties match
     given(mockContext.getBean(eq(KafkaProducer.class), argThat(selfSingedPropertiesMatcher))).willReturn(mockProducer);
     // given the following two schema test files
     var testSchema1 = getResourceAbsPath("com/redhat/schema/pusher/avro/schemas/test_schema.avsc");
     // when invoking the push method with two topics and two schema files
     sut.push(List.of(FAKE_TOPIC1), List.of(testSchema1));
-    // one invocations of the send method
-    then(mockProducer).should().send(argThat(prodRecMatcher(FAKE_TOPIC1, "TestingSchema")::test));
+    // one invocation of the send method with expected arguments including the mocked callback
+    then(mockProducer).should().send(argThat(prodRecMatcher(FAKE_TOPIC1, "TestingSchema")::test), eq(mockCallback));
     // and one invocation for flushing the producer cache
     then(mockProducer).should().flush();
   }
 
   @Test
-  @SuppressWarnings("unchecked")
   void pushing_two_files_and_two_topics_should_result_in_four_producer_records_sent() throws URISyntaxException {
     // stub the cli
     when(mockCli.getKafkaBootstrap()).thenReturn(FAKE_NOT_SECURED_BOOTSTRAP);
@@ -136,10 +136,12 @@ class Avro_schema_pusher_implementation_Test {
     when(mockCli.getNamingStrategy()).thenReturn(FAKE_NAMING_STRATEGY);
     // instantiate the sut with the fake info
     sut = new AvroSchemaPusher(mockCli);
+    // turn off the sut's logger to avoid polluting the build log
+    ((Logger) getField(sut, "LOGGER")).setLevel(Level.OFF);
     // stub the private di context
     setField(sut, "context", mockContext);
-    // stub the producer with a mocked future for any send methd invocation
-    given(mockProducer.send(any(ProducerRecord.class))).willReturn(mock(Future.class));
+    // stub the mocked context to return a mocked callback for the specific arguments
+    given(mockContext.getBean(eq(Callback.class), any(Logger.class), anyString(), anyString())).willReturn(mockCallback);
     // given the di context will return the mocked producer as bean per the properties match
     given(mockContext.getBean(eq(KafkaProducer.class), argThat(notSecuredPropertiesMatcher))).willReturn(mockProducer);
     // given the following two schema test files
@@ -150,7 +152,7 @@ class Avro_schema_pusher_implementation_Test {
     // then producer should be invoked in order
     var mockProdOrder = inOrder(mockProducer);
     // four invocations of the send method, one per schema+topic
-    then(mockProducer).should(mockProdOrder, times(4)).send(prodRecCaptore.capture());
+    then(mockProducer).should(mockProdOrder, times(4)).send(prodRecCaptore.capture(), eq(mockCallback));
     // and one invocation for flushing the producer cache
     then(mockProducer).should(mockProdOrder).flush();
     // verify each schema was sent to each topic (schema names are from in the schema files)
@@ -169,7 +171,7 @@ class Avro_schema_pusher_implementation_Test {
   }
 
   private ArgumentMatcher<Properties> securedPropertiesMatcher =
-      p -> FAKE_NOT_SECURED_BOOTSTRAP_CLEAN.equals(p.getProperty("bootstrap.servers"))
+      p -> FAKE_SECURED_BOOTSTRAP.equals(p.getProperty("bootstrap.servers"))
             && (FAKE_REGISTRY + "apis/ccompat/v6").equals(p.getProperty("schema.registry.url"))
             && "SSL".equals(p.getProperty("security.protocol"))
             && "all".equals(p.getProperty("acks"))
